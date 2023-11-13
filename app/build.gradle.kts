05a928a6ee8845d5fc877cff791fa1e57c7f5023
abhinav3254 <abhinav3254@gmail.com>
2023-11-13 23:34:15 +0530

    pie chart added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,10 @@ dependencies {
     // https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
     implementation("org.apache.poi:poi-ooxml:5.2.3")
 
+//    for pie char
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
