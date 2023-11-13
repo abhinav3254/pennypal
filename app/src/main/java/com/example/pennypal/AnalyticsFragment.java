54dd2c5d730423a18515cbc960d5aba8268b4aff
abhinav3254 <abhinav3254@gmail.com>
2023-11-13 23:25:39 +0530

    analytics fragment

--- app/src/main/java/com/example/pennypal/AnalyticsFragment.java
+++ app/src/main/java/com/example/pennypal/AnalyticsFragment.java
@@ -9,11 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class ProfileFragment extends Fragment {
+public class AnalyticsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_analytics, container, false);
     }
 }
