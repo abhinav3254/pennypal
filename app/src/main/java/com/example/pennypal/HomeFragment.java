2c0797bcb9df6de9bf15d25a699ddb3649f772d6
abhinav3254 <abhinav3254@gmail.com>
2023-11-13 10:51:40 +0530

    search added

--- app/src/main/java/com/example/pennypal/HomeFragment.java
+++ app/src/main/java/com/example/pennypal/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.pennypal;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,6 +28,7 @@ public class HomeFragment extends Fragment {
     private MyAdapter adapter;
     private List<Expense> expenseList;
     private DatabaseHelper databaseHelper;
+    private EditText searchEditText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +36,7 @@ public class HomeFragment extends Fragment {
         initializeViews(view);
         setupRecyclerView();
         retrieveExpenses();
+        setupSearchEditText();
         return view;
     }
 
@@ -41,6 +47,7 @@ public class HomeFragment extends Fragment {
      */
     private void initializeViews(View view) {
         recyclerView = view.findViewById(R.id.recyclerView);
+        searchEditText = view.findViewById(R.id.searchEditText);
     }
 
     /**
@@ -59,5 +66,47 @@ public class HomeFragment extends Fragment {
     private void retrieveExpenses() {
         expenseList = databaseHelper.getAllExpenses();
         adapter.updateData(expenseList);
+    }
+
+    /**
+     * Sets up a TextWatcher for the search input field to filter expenses based on user input.
+     */
+    private void setupSearchEditText() {
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // Do something before text changes
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // Do something when text changes
+                Log.d("TEXT3254", "onTextChanged: ");
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Do something after text changes
+                String searchText = editable.toString();
+                filterExpenses(searchText);
+            }
+        });
+    }
+
+    /**
+     * Filters expenses based on the search text and updates the adapter.
+     *
+     * @param searchText The text entered by the user in the search field.
+     */
+    private void filterExpenses(String searchText) {
+        // Implement your logic to filter expenses based on searchText
+        // For example, you can update the expenseList and call adapter.updateData(expenseList)
+        Log.d("afterTextChanged3254", "filterExpenses: " + searchText);
+
+        // Use the searchExpenses method to get filtered results
+        List<Expense> filteredExpenses = databaseHelper.searchExpenses(searchText);
+
+        // Update the adapter with the filtered data
+        adapter.updateData(filteredExpenses);
     }
 }
